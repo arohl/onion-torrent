83e50720aa46b02890d0121a91cd087e929fbc39
Andrew Rohl <a.rohl@curtin.edu.au>
2017-12-25 23:15:34 +0800

    Added time display

--- torrent.py
+++ torrent.py
@@ -12,6 +12,8 @@ while True:
   statvfs = os.statvfs(USB_MOUNT)
   total = float(statvfs.f_blocks) * statvfs.f_frsize / 1000000000
   free = float(statvfs.f_bfree) * statvfs.f_frsize / 1000000000
-  oledExp.write('Free: {:.3}/{:.3} GB'.format(free, total))
   oledExp.setCursor(0, 0)
+  oledExp.write(time.strftime("%H:%M:%S"))
+  oledExp.setCursor(1, 0)
+  oledExp.write('Free: {:.3}/{:.3} GB'.format(free, total))
   time.sleep(5)
