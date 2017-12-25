2a317d5c7463fdde7606c0d00f7954ff6fd6e042
Andrew Rohl <a.rohl@curtin.edu.au>
2017-12-25 23:19:42 +0800

    Improved time display

--- torrent.py
+++ torrent.py
@@ -13,7 +13,7 @@ while True:
   total = float(statvfs.f_blocks) * statvfs.f_frsize / 1000000000
   free = float(statvfs.f_bfree) * statvfs.f_frsize / 1000000000
   oledExp.setCursor(0, 0)
-  oledExp.write(time.strftime("%H:%M:%S"))
+  oledExp.write('Time: ' + time.strftime("%H:%M:%S"))
   oledExp.setCursor(1, 0)
   oledExp.write('Free: {:.3}/{:.3} GB'.format(free, total))
   time.sleep(5)
